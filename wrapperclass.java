bb00b93d0724e52c0a12734e9fb9243b3585d0b8
Srigowri Deogirikar <32318383+srigowrideo@users.noreply.github.com>
2021-07-09 15:43:50 +0530

    Add files via upload

---
+++ wrapperclass.java
@@ -0,0 +1,13 @@
+package fibonacciseries;
+
+public class wrapperclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Integer x=5;int X;
+X=x+10;
+System.out.println(X);
+	
+	}
+
+}
